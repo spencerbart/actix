9ef47202b406f14f14a0cc1cca77f9f6defe0137
Spencer Bartholomew <38776747+konga400@users.noreply.github.com>
2022-08-07 23:17:07 -0600

    update ports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
@@ -16,13 +17,15 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port_string: String = env::var("PORT").unwrap_or("8080".to_string());
+    let port_number: u16 = port_string.parse().unwrap();
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", port_number))?
     .run()
     .await
 }
